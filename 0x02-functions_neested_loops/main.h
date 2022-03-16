b591b5df99eeb9daff87f1c2786b76611fb849c5
YisihakAb <yisihakabraham51@gmail.com>
2022-03-16 07:53:57 -0700

    main file

--- 0x02-functions_neested_loops/main.h
+++ 0x02-functions_neested_loops/main.h
@@ -1,7 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
-void print_alphabet(void)
-void print_alphabet_x10(void);
-int is_lower(int c);
+void print_alphabet(void);
+void print_alphbet_x10(void);
+int _islower(int c)
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
@@ -10,3 +12,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+#endif
