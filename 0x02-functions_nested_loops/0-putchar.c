2142b590ec34410b087410dd909ef2dcb0909895
YisihakAb <yisihakabraham51@gmail.com>
2022-03-16 08:51:20 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ *
+ * Return:
+ */
+int main(void)
+{
+	char yise[] = "_putchar";
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(yise[c]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
